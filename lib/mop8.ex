2e67580a3d720a3ceff6d20181e7af47d28e15c7
mopp <hello@mopp.jp>
2021-04-10 01:11:13 +0900

    add update_gram_map

--- lib/mop8.ex
+++ lib/mop8.ex
@@ -38,4 +38,33 @@ defmodule Mop8 do
   defp bigram([x | [y | _] = rest], acc) do
     bigram(rest, [x <> y | acc])
   end
+
+  def update_gram_map(gram_map, []) do
+    gram_map
+  end
+
+  def update_gram_map(gram_map, [_]) do
+    gram_map
+  end
+
+  def update_gram_map(gram_map, [x | [y | _] = rest]) do
+    {_, gram_map} =
+      Map.get_and_update(gram_map, x, fn
+        nil ->
+          {nil, %{y => 1}}
+
+        count_map ->
+          Map.get_and_update(count_map, y, fn
+            nil ->
+              # Initialize.
+              {nil, 1}
+
+            v ->
+              # Increment the count.
+              {v, v + 1}
+          end)
+      end)
+
+    update_gram_map(gram_map, rest)
+  end
 end
