7a3b54d1ffadff850b225ed7b9c6868d85292474
mopp <hello@mopp.jp>
2021-04-09 23:38:38 +0900

    init

---
+++ lib/mop8/application.ex
@@ -0,0 +1,20 @@
+defmodule Mop8.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Mop8.Worker.start_link(arg)
+      # {Mop8.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Mop8.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/mop8_test.exs
@@ -0,0 +1,8 @@
+defmodule Mop8Test do
+  use ExUnit.Case
+  doctest Mop8
+
+  test "greets the world" do
+    assert Mop8.hello() == :world
+  end
+end
