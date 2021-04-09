05679d73b7cc43c4294d6524ed0472f1e0a2e96e
mopp <hello@mopp.jp>
2021-04-10 02:06:54 +0900

    add construct_sentence

--- test/mop8_test.exs
+++ test/mop8_test.exs
@@ -90,4 +90,56 @@ defmodule Mop8Test do
 
     assert(0 == map_size(Mop8.update_gram_map(%{}, [])))
   end
+
+  test "construct_sentence" do
+    :rand.seed(:exrop, {123, 44, 55})
+
+    bigram = Mop8.bigram("今日はいい天気ですね。")
+    gram_map = Mop8.update_gram_map(%{}, bigram)
+
+    assert("はいい天気ですね。" == Mop8.construct_sentence(gram_map))
+
+    source_sentences = [
+      "今日はいい天気でしたね。",
+      "明日はいい天気でしたね。",
+      "昨日はいい天気でしたよ。"
+    ]
+
+    gram_map =
+      source_sentences
+      |> Enum.map(&Mop8.bigram/1)
+      |> Enum.reduce(gram_map, &Mop8.update_gram_map(&2, &1))
+
+    assert("日はいい天気でしたね。" == Mop8.construct_sentence(gram_map))
+  end
+
+  test "do_roulette_selection" do
+    pairs = [
+      {:a, 1},
+      {:b, 1},
+      {:c, 1}
+    ]
+
+    assert(:a == Mop8.do_roulette_selection(pairs, 1))
+    assert(:b == Mop8.do_roulette_selection(pairs, 2))
+    assert(:c == Mop8.do_roulette_selection(pairs, 3))
+
+    pairs = [
+      {:a, 2},
+      {:b, 10},
+      {:c, 2}
+    ]
+
+    assert(:b == Mop8.do_roulette_selection(pairs, 3))
+    assert(:b == Mop8.do_roulette_selection(pairs, 12))
+
+    pairs = [
+      {:a, 10},
+      {:b, 10},
+      {:c, 10}
+    ]
+
+    assert(:a == Mop8.do_roulette_selection(pairs, 10))
+    assert(:b == Mop8.do_roulette_selection(pairs, 19))
+  end
 end
