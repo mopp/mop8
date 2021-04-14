64a24ac3ea5e771ace0c23b848ca12589eaa4c11
mopp <hello@mopp.jp>
2021-04-14 22:27:10 +0900

    implement WebSockex callbacks

--- lib/mop8/slack/bot.ex
+++ lib/mop8/slack/bot.ex
@@ -1,4 +1,5 @@
 defmodule Mop8.Slack.Bot do
+  require Logger
   use WebSockex
 
   def start_link(url) do
@@ -6,12 +7,45 @@ defmodule Mop8.Slack.Bot do
   end
 
   @impl WebSockex
-  def handle_frame({_type, _msg}, state) do
+  def handle_connect(_conn, state) do
+    Logger.info("Connected.")
+
+    {:ok, state}
+  end
+
+  @impl WebSockex
+  def handle_disconnect(connection_status_map, state) do
+    Logger.info("Disconnected. #{inspect(connection_status_map)}")
+
+    {:ok, state}
+  end
+
+  @impl WebSockex
+  def handle_frame({:text, message}, state) do
+    message = Poison.decode!(message)
+    IO.inspect("frame received. message: #{inspect(message)}")
+
     {:ok, state}
   end
 
   @impl WebSockex
   def handle_cast({:send, {_type, _msg} = frame}, state) do
+    IO.inspect("handle_cast: #{inspect(frame)}")
+
     {:reply, frame, state}
+  end
+
+  @impl WebSockex
+  def handle_info(msg, state) do
+    IO.inspect("handle_info: #{inspect(msg)}")
+
+    {:ok, state}
+  end
+
+  @impl WebSockex
+  def terminate(reason, _state) do
+    IO.inspect("terminate. reason: #{inspect(reason)}")
+
+    :ok
   end
 end
