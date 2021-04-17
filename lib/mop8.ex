7226a7a96abb3f533d57115bce733350b31308d2
mopp <hello@mopp.jp>
2021-04-17 16:34:37 +0900

    remove unused code and test

--- lib/mop8.ex
+++ lib/mop8.ex
@@ -1,18 +1,2 @@
 defmodule Mop8 do
-  @moduledoc """
-  Documentation for `Mop8`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mop8.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/mop8_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Mop8Test do
-  use ExUnit.Case
-  doctest Mop8
-
-  test "greets the world" do
-    assert Mop8.hello() == :world
-  end
-end
