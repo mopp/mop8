2cfa77e801c48f57f169483e11f7b618d1c09700
mopp <hello@mopp.jp>
2021-04-18 22:31:32 +0900

    add BotTest

---
+++ test/mop8/bot_test.exs
@@ -0,0 +1,27 @@
+defmodule Mop8.BotTest do
+  use ExUnit.Case, async: true
+
+  alias Mop8.WordMap
+  alias Mop8.Bot
+
+  test 'handle_message/3' do
+    word_map = WordMap.new()
+    config = Bot.Config.new("test_user_id", "test_bot_user_id")
+
+    message = {"other_user_id", "hi", 123}
+    assert {:ok, :ignore} == Bot.handle_message(word_map, message, config)
+
+    message = {"test_user_id", "hi", 123}
+    assert {:ok, {:update, _}} = Bot.handle_message(word_map, message, config)
+
+    message = {"test_user_id", "hi with <http://example.com/hoge>", 123}
+    assert {:ok, {:update, _}} = Bot.handle_message(word_map, message, config)
+
+    message = {"target_user_id", "<@test_bot_user_id> hi", 123}
+    assert {:ok, :ignore} == Bot.handle_message(word_map, message, config)
+
+    word_map = WordMap.put(word_map, ["あい", "うえ"])
+    message = {"target_user_id", "<@test_bot_user_id> hi", 123}
+    assert {:ok, {:reply, _}} = Bot.handle_message(word_map, message, config)
+  end
+end
