82fa96b26cd9111970e0ebea596bc914ba5b28ca
mopp <hello@mopp.jp>
2021-04-30 15:30:17 +0900

    add Console.say/1

--- lib/mop8/console.ex
+++ lib/mop8/console.ex
@@ -2,12 +2,16 @@ defmodule Mop8.Console do
   alias Mop8.Adapter.MessageController
 
   def mention() do
+    say("<@#{System.fetch_env!("BOT_USER_ID")}>うみゃー")
+  end
+
+  def say(text) when is_binary(text) do
     %{
       "type" => "event_callback",
       "event" => %{
         "type" => "message",
-        "user" => "test_user_id",
-        "text" => "<@#{System.fetch_env!("BOT_USER_ID")}>うみゃー",
+        "user" => System.fetch_env!("TARGET_USER_ID"),
+        "text" => text,
         "channel" => "test_channel_id",
         "event_ts" => "0"
       }
