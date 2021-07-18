b89bc0775887312ffbc2ebae2748351033da715f
mopp <hello@mopp.jp>
2021-07-18 12:27:05 +0900

    upgrade Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mop8.MixProject do
     [
       app: :mop8,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
